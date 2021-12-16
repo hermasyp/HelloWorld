027959f80e88c09caf5da83b9beeca875a300d16
JordJ19 <jordie.juwono96@gmail.com>
2021-12-16 22:28:42 +0700

    create new feature class jordie

---
+++ src/main/kotlin/Jordie.kt
@@ -0,0 +1,10 @@
+class Jordie {
+    val name = "Jordie"
+    val age = 25
+    fun writeMyName(){
+        println("==================")
+        println("Hello ! my name is $name !")
+        println("I'm $age Years Old!")
+        println("==================")
+    }
+}
