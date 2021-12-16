1f1e85ee6f3d7c9e2af292fb00e2a808143f0e2a
Zuifany <zee.fanny7@gmail.com>
2021-12-16 23:32:43 +0800

    create new feature class zui

---
+++ src/main/kotlin/Zui.kt
@@ -0,0 +1,10 @@
+class Zui {
+    val name = "Zui"
+    val age = 30
+    fun writeMyName(){
+        println("====================")
+        println("Hello! My Name is $name !")
+        println("I'm $age Years old!")
+        println("=====================")
+    }
+}
