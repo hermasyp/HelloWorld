5ecef30c3bfe99643ac548c9ecc69fc5df1f45a0
Gryan <gryangia55@gmail.com>
2021-12-16 22:28:20 +0700

    create new feature class gryan

---
+++ src/main/kotlin/Gryan.kt
@@ -0,0 +1,10 @@
+class Gryan {
+    val name = "Gryan"
+    val age = 21
+    fun writeMyName(){
+        println("======================")
+        println("Hello My Name is $name !")
+        println("I'm $age Years Old!")
+        println("======================")
+    }
+}
