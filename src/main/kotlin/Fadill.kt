ceb6313cff7dda99630d4f8e174fbbf92caa6406
fadill20 <fadillah20rahmad@gmail.com>
2021-12-17 18:29:34 +0700

    create new feature class fadill

---
+++ src/main/kotlin/Fadill.kt
@@ -0,0 +1,11 @@
+class Fadill {
+    val name = "'Fadill"
+    val age = 35
+    fun writeMyName(){
+        println("=================================")
+        println("Hello ! my name is $name !")
+        println("I' m $age years old")
+        println("=================================")
+
+    }
+}
