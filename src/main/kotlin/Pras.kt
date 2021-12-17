61f589aadd1662dbf03db045d36bc9620a0be96c
prasetyo <prasuetyo@gmail.com>
2021-12-17 19:48:55 +0700

    create new feature class pras

---
+++ src/main/kotlin/Pras.kt
@@ -0,0 +1,11 @@
+class Pras {
+    val name = "Pras"
+    val age = 29
+    fun writeMyName(){
+        println("===============================")
+        println("Hello ! My Name Is $name !")
+        println("I 'm $age Years old!")
+        println("================================")
+
+    }
+}
