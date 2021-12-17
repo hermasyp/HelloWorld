9a033b44c2ca6e6a23d0194e2f69f00e252197f3
Thomasaja <95894629+Thomasaja@users.noreply.github.com>
2021-12-17 15:25:23 +0700

    create new feature class thomas

---
+++ src/main/kotlin/Thomas.kt
@@ -0,0 +1,10 @@
+class Thomas {
+    val name = "Thomas"
+    val age = 20
+    fun writeMyName(){
+        println("===================")
+        println("Hello ! my name is $name !")
+        println("I'm $age Years old!")
+        println("===================")
+    }
+}
