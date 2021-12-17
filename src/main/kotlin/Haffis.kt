6f769d7d5af312d8581fc34915f7efdf538f51c3
haffiseza <haffiseza@gmail.com>
2021-12-17 20:05:18 +0700

    creating class haffis

---
+++ src/main/kotlin/Haffis.kt
@@ -0,0 +1,10 @@
+class Haffis {
+    val name = "Haffis"
+    val age = 25
+    fun writeMyName(){
+        println("=================")
+        println("Hello ! my name is $name !")
+        println("I'm $age Years old!")
+        println("=================")
+    }
+}
