2e001a785c8fdafbf2c7f5e1d4187d37f70fb67c
sitiruqayah <sitiruqayah161295@gmail.com>
2021-12-17 19:58:19 +0700

    Siti Ruqayah

---
+++ src/main/kotlin/SitiRuqayah.kt
@@ -0,0 +1,10 @@
+class SitiRuqayah {
+   val name = "Siti"
+    val age = 26
+    fun writeMyName () {
+        println("=================")
+        println("Hello! My Name is $name!")
+        println("I'm $age years old!")
+        println("=================")
+    }
+}
