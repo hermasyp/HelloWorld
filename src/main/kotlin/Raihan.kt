392a319d276b8d3b15124b17f17ede6526fd2df2
raihan <raihanlatjeno0@gmail.com>
2021-12-20 15:35:52 +0700

    create new feature class raihan

---
+++ src/main/kotlin/Raihan.kt
@@ -0,0 +1,11 @@
+class Raihan {
+    val name = "Riahan"
+    val age = 19
+
+    fun writeMyName(){
+        println("===================")
+        println("Hello ! may name is $name !")
+        println("I'm $age Years old!")
+        println("===================")
+    }
+}
